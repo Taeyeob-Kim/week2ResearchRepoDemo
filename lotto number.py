c73086833a6b12489f22ef279aab816faefca04d
20220876 <110364597+Taeyeob-Kim@users.noreply.github.com>
2022-08-24 17:44:32 +1200

    Update lotto number.py

--- lotto number.py
+++ lotto number.py
@@ -6,3 +6,5 @@ while len(lotto)<6:
         lotto.append(num)
     
     print("the lotto numbers are", lotto)
+    
+    ## I bought a lotto last night and It passed my mind, I may could make code to pick numbers. And this is what I made. 
